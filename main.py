f51e11f9e8f40677a819cb772f937cde8f7004e0
MaxTheTimeMaster <34436202+MaxTheTimeMaster@users.noreply.github.com>
2017-12-14 12:45:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print ('hello!')
+from tkinter import *
+def create_screen():
+  global root
+  root = Tk() # Называем окно
+  root.geometry("400x500") # Размеры экрана 400 на 5000
+def Objects():
+  size = "?"
+  size1 = "?"
+  class buttons(width, height, selfs):
+    def __init__(self, Class, width, height):
+      self.width1 = width
+      self.height1 = height
+      self.Class = Button(root, width1, height1)
+  button1 = class()
+     
+      
+  button_1 = Button(root, width = size1, height = size) # Задай значение высоты и ширины кнопок
+  button_2 = Button(root, width = size1, height = size)
+  button_3 = Button(root, width = size1, height = size)
+  button_4 = Button(root, width = size1, height = size)
+  button_5 = Button(root, width = size1, height = size)
+  button_6 = Button(root, width = size1, height = size)
+  button_7 = Button(root, width = size1, height = size)
+  button_8 = Button(root, width = size1, height = size)
+  button_9 = Button(root, width = size1, height = size)
+  button_0 = Button(root, width = size1, height = size)
+  button_plus = Button(root, width = size1, height = size)
+  button_minus = Button(root, width = size1, height = size)
+  button_divide = Button(root, width = size1, height = size)
+  button_multyply = Button(root, width = size1, height = size)
+  button_make = Button(root, width = size1, height)
+  
+  
+  
+  
+create_screen()
+root.mainloop()
